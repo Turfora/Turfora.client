b33695d0fe5619a5e2ae0fa3b8b0b58e5519ae05
Kartikey Pandey <95975584+ikartikeyo@users.noreply.github.com>
2026-03-13 17:15:46 +0530

    Revert "[WIP] Add comprehensive login and signup pages for Turfora application"

--- src/app/navigation/mainNavigator.tsx
+++ src/app/navigation/mainNavigator.tsx
@@ -1,10 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import LoginScreen from "../../features/auth/screens/loginScreen"
-import SignupScreen from "../../features/auth/screens/signupScreen"
 import UserHome from "../../features/user/screens/userHome"
-import OwnerDashboard from "../../features/owner/screens/ownerDashboard"
-import AdminDashboard from "../../features/admin/screens/adminDashboard"
 
 const Stack = createNativeStackNavigator()
 
@@ -13,10 +10,7 @@ export default function MainNavigator() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="UserHome" component={UserHome} />
-        <Stack.Screen name="OwnerDashboard" component={OwnerDashboard} />
-        <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   )
